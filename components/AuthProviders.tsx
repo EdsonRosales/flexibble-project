10c10d2c273898c9c12b54552afe051b69587e60
EdsonRosales97 <erosales@applab.mx>
2023-07-13 17:18:08 -0600

    feat[AuthProvider]: Providers types for authentication

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+"use client"
+
+import { getProviders, signIn } from 'next-auth/react'
+import { useState, useEffect } from 'react'
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+}
+
+type Providers = Record <string, Provider>;
 
 function AuthProviders() {
+
+  const [providers, setProviders] = useState<Providers | null>(null);
+
   return (
     <div>AuthProviders</div>
   )
