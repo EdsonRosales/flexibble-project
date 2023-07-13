cb333c905a3763eb9461a494226eeae36c5d39d6
EdsonRosales97 <erosales@applab.mx>
2023-07-13 15:57:27 -0600

    feat[Navbar]: Basic Navbar makeup created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { NavLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
+import AuthProviders from './AuthProviders';
 
 function Navbar() {
+  const session = {};
+
   return (
     <nav className='flexBetween navbar'>
         <div className='flex-1 flexStart gap-10'>
@@ -21,6 +24,21 @@ function Navbar() {
                     </Link>
                 ))}
             </ul>
+        </div>
+
+        <div className='flexCenter gap-4'>
+            {session ? (
+                <>
+                    UserPhoto
+
+                    <Link href="/create-project">
+                        Share Work
+                    </Link>
+                </>
+            ) : (
+                /** Manage the user session */
+                <AuthProviders />
+            )}
         </div>
     </nav>
   )
