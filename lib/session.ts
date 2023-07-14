aead4acdbcc2f0cbd07595d935a74d314af172ef
EdsonRosales97 <erosales@applab.mx>
2023-07-14 16:41:26 -0600

    feat[Auth]: GoogleProvider for user authentication setup

--- lib/session.ts
+++ lib/session.ts
@@ -11,18 +11,18 @@ import { JWT } from "next-auth/jwt";
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
     ],
-    jwt: {
-        encode: ({ secret, token }) => {
+    // jwt: {
+    //     encode: ({ secret, token }) => {
 
-        },
-        decode: async ({ secret, token }) => {
+    //     },
+    //     decode: async ({ secret, token }) => {
 
-        }
-    },
+    //     }
+    // },
     theme: {
         colorScheme: 'light',
         logo: '/logo.png'
@@ -30,10 +30,20 @@ export const authOptions: NextAuthOptions = {
     callbacks: {
         /** This function has triggered everytime that the user visits the page */
         async session({ session }) {
-
+            return session;
         },
-        async signIn({ user }) {
-            
+        async signIn({ user }: { user: AdapterUser | User }) {
+            try {
+                // Get the user of the BD if they exist
+
+                // if they don't exist, create them
+
+                // finally return the user
+                return true
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
         }
     }
 }
