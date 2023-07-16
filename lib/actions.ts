33d062aaa941b31e0e12ef74b7239b624cbdd9d8
EdsonRosales97 <erosales@applab.mx>
2023-07-15 20:30:41 -0600

    safe[Auth]: Safe commit

---
+++ lib/actions.ts
@@ -0,0 +1,9 @@
+import { GraphQLClient } from "graphql-request";
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+    try {
+        // client.request...
+    } catch (error) {
+        throw error
+    }
+};
