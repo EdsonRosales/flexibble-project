0f9a2f1c37eae8524b811bd3d4d59428172d0b89
Edson Rosales <edsonrosales@Edsons-MacBook-Pro.local>
2023-07-17 13:49:40 -0600

    feat[Graphql]: Actions, mutations & querys setup to connect to the server of Grafbase with Graphql querys

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,9 +1,32 @@
+import { createUserMutation, getUserQuery } from "@/graphql";
 import { GraphQLClient } from "graphql-request";
+
+const isProduction = process.env.NODE_ENV === 'production';
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql'; // <--- Endpoint to dev or prod environment
+const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';   // <--- Key to dev or prod environment
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';  // <--- Check the correct server
+
+const client = new GraphQLClient(apiUrl);
 
 const makeGraphQLRequest = async (query: string, variables = {}) => {
     try {
-        // client.request...
+        return await client.request(query, variables);
     } catch (error) {
         throw error
     }
+};
+
+// Action to get the User
+export const getUser = (email: string) => {
+    return makeGraphQLRequest(getUserQuery, { email });
+};
+
+// Action to create a User
+export const createUser = (name: string, email: string, avatarUrl: string) => {
+    const variables = {
+        input: {
+            name, email, avatarUrl
+        }
+    };
+    return makeGraphQLRequest(createUserMutation, variables);
 };
--- lib/session.ts
+++ lib/session.ts
@@ -7,7 +7,8 @@ import { AdapterUser } from 'next-auth/adapters';
 import GoogleProvider from "next-auth/providers/google";
 import jsonwebtoken from "jsonwebtoken";
 import { JWT } from "next-auth/jwt";
-import { SessionInterface } from "@/common.types";
+import { SessionInterface, UserProfile } from "@/common.types";
+import { createUser, getUser } from "./actions";
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -31,13 +32,33 @@ export const authOptions: NextAuthOptions = {
     callbacks: {
         /** This function has triggered everytime that the user visits the page */
         async session({ session }) {
-            return session;
+            const email = session?.user?.email as string;
+
+            try {
+                const data = await getUser(email) as { user?: UserProfile }
+                // Multiple spreading to combine the two sessions in one
+                const newSession = {
+                    ...session,
+                    user: {
+                        ...session.user,
+                        ...data?.user
+                    }
+                }
+                return newSession;
+            } catch (error) {
+                console.error('Error retrieving user data', error);
+                return session;
+            }
         },
         async signIn({ user }: { user: AdapterUser | User }) {
             try {
                 // Get the user of the BD if they exist
+                const userExist = await getUser(user?.email as string) as { user?: UserProfile }
 
                 // if they don't exist, create them
+                if(!userExist.user) {
+                    await createUser(user.name as string, user.email as string, user.image as string);
+                }
 
                 // finally return the user
                 return true
