a148037fa6cd6e16cd8541b377ec10a46d7b24c6
Edson Rosales <edsonme9@gmail.com>
2023-07-17 14:00:19 -0600

    changes

--- lib/session.ts
+++ lib/session.ts
@@ -17,14 +17,14 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
     ],
-    // jwt: {
-    //     encode: ({ secret, token }) => {
+    jwt: {
+        encode: ({ secret, token }) => {
 
-    //     },
-    //     decode: async ({ secret, token }) => {
+        },
+        decode: async ({ secret, token }) => {
 
-    //     }
-    // },
+        }
+    },
     theme: {
         colorScheme: 'light',
         logo: '/logo.png'
