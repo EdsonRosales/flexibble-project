a110120024dc8c417f50d835c6e8a978711d8055
Edson Rosales <edsonme9@gmail.com>
2023-07-19 14:07:00 -0600

    feat[Modal]: Setting up the form for a New Project

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+"use client"
 
-function ProjectForm() {
+import { ChangeEvent } from 'react'
+import { SessionInterface } from '@/common.types';
+
+type ProjectFormProps = {
+    type: string,
+    session: SessionInterface
+}
+
+function ProjectForm({ type, session }: ProjectFormProps) {
+
+  const handleFormSubmit = (e: React.FormEvent) => {};
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
+
+  const image = null;
+
   return (
-    <div>ProjectForm</div>
+    <form onSubmit={handleFormSubmit} className='flexstart form'>
+        <div className='flexstart form_image-container'>
+            <label htmlFor='poster' className='flexCenter form_image-label'>
+                {!image && 'Choose a poster for you project'}
+            </label>
+            <input
+                id='image'
+                type='file'
+                accept='image/*'
+                required={type === 'create'}
+                className='form_image-input'
+                onChange={handleChangeImage}
+            />
+        </div>
+    </form>
   )
 }
 
