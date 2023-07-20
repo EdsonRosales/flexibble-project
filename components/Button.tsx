4bb26c9e8e9c469333242ea8475a2af814a3331b
Edson Rosales <edsonme9@gmail.com>
2023-07-20 13:53:25 -0600

    feat[Custom Button]: Dynamic styles for the button added

--- components/Button.tsx
+++ components/Button.tsx
@@ -26,8 +26,10 @@ function Button({
     <button
       type={type || 'button'}
       disabled={isSubmitting}
-      // bgColor & textColor
-      className="flexCenter gap-3 px-4 py-3"
+      className={`flexCenter gap-3 px-4 py-3
+        ${textColor || 'text-white'}
+        ${isSubmitting ? 'bg-black/50' : bgColor || 'bg-primary-purple'}
+         rounded-xl text-sm font-medium max-md:w-full`}
       onClick={handleClick}
     >
       {leftIcon &&
